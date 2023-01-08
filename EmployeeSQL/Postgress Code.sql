0555c32e35bbc69e8ef034ff55112da7f8e4b4ab
L-Nash <leah_nash@icloud.com>
2023-01-07 18:39:43 -0600

    010723

--- EmployeeSQL/Postgress Code.sql
+++ EmployeeSQL/Postgress Code.sql
@@ -1,5 +1,5 @@
 
--- Drop Table Employees;
+Drop Table Employees;
 
 CREATE TABLE Employees (
     "emp_no" INTEGER   NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE Employees (
         "emp_no"
      )
 );
--- Drop Table "Salaries";
+Drop Table "Salaries";
 CREATE TABLE "Salaries" (
     "emp_no" INTEGER   NOT NULL,
     "salary" INTEGER   NOT NULL
@@ -26,36 +26,30 @@ CREATE TABLE "Titles" (
         "title_id"
      )
 );
--- Drop Table "Departments";
+Drop Table "Departments" cascade;
 CREATE TABLE "Departments" (
     "dept_no" Varchar(6)   NOT NULL,
     "dept_name" Varchar(50)   NOT NULL,
     CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
+        "dept_no","dept_name")
 );
--- Drop Table "Dept_emp";
+Drop Table "Dept_emp";
 CREATE TABLE "Dept_emp" (
     "emp_no" INTEGER   NOT NULL,
-    "dept_no" Varchar(4)   NOT NULL
---     CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
---         "emp_no","dept_no"
---      )
-);
--- Drop Table "Dept_manager";
+    "dept_no" Varchar(4)   NOT NULL,
+   );
+   
+Drop Table "Dept_manager";
 CREATE TABLE "Dept_manager" (
     "dept_no" Varchar(4)   NOT NULL,
     "emp_no" INTEGER   NOT NULL,
-    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
-        "dept_no","emp_no"
-     )
-);
+ );
 
--- ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "Dept_emp" ("emp_no");
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
 
--- ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
--- REFERENCES "Employees" ("emp_title_id");
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
+REFERENCES "Employees" ("emp_title_id");
 
 ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
@@ -69,18 +63,63 @@ REFERENCES "Departments" ("dept_no");
 ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
 
--- Select * from "Dept_manager";
--- Select * from "Departments";
--- Select * from "Dept_emp";
--- Select * from "Salaries";
-Select * from "employees";
--- Select * from "Titles";
 
 --1. List the employee number, last name, first name, sex, and salary of each employee.
-Select ("emp_no", "last_name", "first_name", "sex", "salary")
+Select employees.emp_no, employees.last_name, employees.first_name, employees.sex, s.salary
 From employees
+Inner Join "Salaries" as s
+on employees.emp_no = s.emp_no;
 
 --2. List the first name, last name, and hire date for the employees who were hired in 1986.
 
-Select ("first_name", "last_name", "hire_date")
+Select first_name, last_name, hire_date
 From employees where hire_date between '1986-01-01' and '1986-12-31';
+
+
+--3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
+Select dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+From "Dept_manager" as  dm
+Inner Join "Departments" as d
+on dm.dept_no = d.dept_no
+Inner Join "employees" as e 
+on dm.emp_no = e.emp_no;
+
+--4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+Select d.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+From "employees" as e
+Inner Join "Dept_emp" as de 
+on e.emp_no = de.emp_no
+Inner Join "Departments" as d
+on de.dept_no = d.dept_no;
+
+--5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+Select first_name, last_name, sex
+from "employees"
+where first_name = 'Hercules' and last_name like 'B%';
+
+--6. List each employee in the Sales department, including their employee number, last name, and first name.
+Select emp_no, last_name, first_name
+from "employees"
+Where emp_no in (Select emp_no
+				from "Dept_emp"
+				Where dept_no in
+					(Select dept_no
+					from "Departments"
+					Where dept_name = 'Sales'));
+					
+--7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+Select e.emp_no, e.last_name, e.first_name, d.dept_name
+from "employees" e
+inner join "Dept_emp" de
+on e.emp_no = de.emp_no
+inner join "Departments" d 
+on de.dept_no = d.dept_no
+Where dept_name = 'Sales' or dept_name ='Development';
+
+--8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+Select last_name, count(last_name)
+from employees
+group by last_name
+order by count(last_name) desc;
